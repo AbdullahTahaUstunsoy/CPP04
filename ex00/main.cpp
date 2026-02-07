fcc0b31cb9221e787539a57e2c98e84e2f1dad6b
Abdullah Taha Ustunsoy <abdullahustunsoy1@gmail.com>
2026-02-07 21:10:52 +0300

    ex00 completed

---
+++ ex00/main.cpp
@@ -0,0 +1,33 @@
+#include "WrongCat.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "----- Correct implementation test: Testing Animal, Cat, and Dog -----" << std::endl;
+    const Animal* meta = new Animal();
+    const Animal* i = new Cat();
+    const Animal* j = new Dog();
+    std::cout << meta->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    std::cout << j->getType() << " " << std::endl;
+    meta->makeSound();
+    i->makeSound();
+    j->makeSound();
+
+    delete meta;
+    delete i;
+    delete j;
+
+    std::cout << "----- Wrong implementation test: WrongAnimal and WrongCat -----" << std::endl;
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongI = new WrongCat();
+    std::cout << wrongMeta->getType() << " " << std::endl;
+    std::cout << wrongI->getType() << " " << std::endl;
+    wrongMeta->makeSound();
+    wrongI->makeSound();
+
+    delete wrongMeta;
+    delete wrongI;
+}
